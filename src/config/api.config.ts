f069abaf3fc61a3cfd0c1cdbbf4349f51af8b5e1
Bojan Crnovcic <bojan969@gmail.com>
2024-05-24 17:31:13 +0200

    backend config

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const ApiConfig = {
-    API_URL: 'https://social-network-app-nine.vercel.app/',
+    API_URL: 'https://social-network-app-production.up.railway.app/',
     PHOTO_PATH: 'https://www.dropbox.com/home/Apps/social_555'
 }
